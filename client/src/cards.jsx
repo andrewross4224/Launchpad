c48e06e6b1cae27d7b89a3bb0490fd3b21be6850
gooseman789 <106124000+gooseman789@users.noreply.github.com>
2024-02-07 21:03:46 -0500

    adding card.jsx

---
+++ client/src/cards.jsx
@@ -0,0 +1,20 @@
+
+return (
+    <li className="card">
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems:'center'}}>
+            <button type="radio" className="star"></button>
+            <div style={{ display: 'relative', }}>
+                <img src="" alt="" />
+            </div>
+            <div className="">
+                <h5>Location</h5>
+                <h5>Spacecraft</h5>
+                <h5>Date</h5>
+                <h5>Time</h5>
+            </div>
+            <div className="">
+                <button onClick="" variant="outline-info">Launch Details</button>
+            </div>
+        </div>
+    </li>
+)

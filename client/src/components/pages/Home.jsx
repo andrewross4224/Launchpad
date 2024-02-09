64a6669a1e59e013a8cce24b9acda213ce77ec5f
gooseman789 <106124000+gooseman789@users.noreply.github.com>
2024-02-08 19:31:18 -0500

    fixing pages and style

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,5 +1,24 @@
+import Card from '../../cards'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
 export default function Home() {
-    return(
-        <h1>Hello</h1>
+    return (
+        <Container>
+            <Row>
+                <Col xs={12} md={4} className='p-3'>
+                    <Card />
+                </Col>
+                <Col xs={12} md={4} className='p-3'>
+                    <Card />
+                </Col>
+                <Col xs={12} md={4} className='p-3'>
+                    <Card />
+                </Col>
+            </Row>
+        </Container>
     )
 }

85a0640373e7c244f524714f358a6229367fd65d
Andrew Ross <andrew.ross4224@gmail.com>
2024-02-14 14:22:32 -0500

    changed css for loader and deleted api calls from utils folder

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -3,11 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-import queryLaunches from '../../utils/launchAPI';
-
-import axios from 'axios';
 
 
 export default function Home() {
@@ -16,9 +14,8 @@ export default function Home() {
     const [results, setResults] = useState({});
 
     const callAPI = async (query) => {
-        const searchResult = await axios.get('https://lldev.thespacedevs.com/2.2.0/launch/upcoming/?limit=100')
+        const searchResult = await axios.get('https://lldev.thespacedevs.com/2.2.0/launch/upcoming/?limit=25')
         setResults(searchResult.data.results);
-        setLoading(false)
     };
 
     useEffect(() => {
@@ -32,7 +29,9 @@ export default function Home() {
     return (
         <main>
             {loading ?
-                (<div className="moon">
+                (<>
+                <h1 className='loading'>Loading your launches!<br/> (this will take a second)</h1>
+                <div className="moon">
                     <img src="https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/space-rocket-512.png" style={{"height" : "20px", "width" : "30px"}} className="loaderRocket" />
                     <ul>
                         <li className='rocket rocket1'></li>
@@ -43,7 +42,8 @@ export default function Home() {
                         <li className='rocket rocket6'></li>
                         <li className='rocket rocket7'></li>
                     </ul>
-                </div>)
+                </div>
+                </>)
                 :
                 (<Container results={results}>
                     <a href={results}>click this</a>
--- client/src/utils/launchAPI.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const queryLaunches = (location) => {
-    axios.get('https://lldev.thespacedevs.com/2.2.0/launch/upcoming/?limit=25').catch(function (error) {
-        console.log(error.toJSON());
-      });
-}
-
-export default queryLaunches;

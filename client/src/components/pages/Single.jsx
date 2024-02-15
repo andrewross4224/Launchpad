85414bca98771d8a4404a06d8ee72be729a78a51
gooseman789 <106124000+gooseman789@users.noreply.github.com>
2024-02-14 21:21:21 -0500

    working on the launch details page

--- client/src/components/pages/Single.jsx
+++ client/src/components/pages/Single.jsx
@@ -3,13 +3,48 @@ import Dropdown from '../Dropdown'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Detail from '../Detail';
 
+import Detail from '../Detail';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+
+    const [loading, setLoading] = useState(true);
+    const [results, setResults] = useState({});
+
+    const callAPI = async (query) => {
+        const searchResult = await axios.get('https://api.openweathermap.org/data/2.5/weather?lat=28.440797968127775&lon=-80.57989329685985&appid=b364c079aaefaadfbc5f1d3a45eebea4&units=imperial')
+        setResults(searchResult);
+        setLoading(false)
+    };
+
+    useEffect(() => {
+        console.log(results);
+    }, [loading]);
+
+    useEffect(() => {
+        callAPI();
+        console.log(results)
+    }, []);
     return (
+        <main>
+            {loading ?
+                (<>
+                <h1 className='loading'>Loading your launches!<br/> (this will take a second)</h1>
+                <div className="moon">
+                    <img src="https://cdn2.iconfinder.com/data/icons/thesquid-ink-40-free-flat-icon-pack/64/space-rocket-512.png" style={{"height" : "20px", "width" : "30px"}} className="loaderRocket" />
+                    <ul>
+                        <li className='rocket rocket1'></li>
+                        <li className='rocket rocket2'></li>
+                        <li className='rocket rocket3'></li>
+                        <li className='rocket rocket4'></li>
+                        <li className='rocket rocket5'></li>
+                        <li className='rocket rocket6'></li>
+                        <li className='rocket rocket7'></li>
+                    </ul>
+                </div>
+                </>) : (  
         <Container className='top'>
             <Row>
                 <Col className='p-3'>
@@ -17,5 +52,5 @@ export default function Home() {
                 </Col>
             </Row>
         </Container>
-    )
+    )} </main>)
 }
